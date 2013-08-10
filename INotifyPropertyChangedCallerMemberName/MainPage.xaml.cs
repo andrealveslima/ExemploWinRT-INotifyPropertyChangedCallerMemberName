339abc0b29afe64fd0e0cf662a5c5938760b4ab7
andrealveslima <andrealveslimeira@gmail.com>
2013-08-09 21:17:18 -0300

    Adicionando INotifyPropertyChangedCallerMemberName

---
+++ INotifyPropertyChangedCallerMemberName/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace INotifyPropertyChangedCallerMemberName
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Pessoa pessoa = new Pessoa();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = pessoa;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            pessoa.Nome = RandomString(10);
+            pessoa.Sobrenome = RandomString(10);
+        }
+
+        private static Random random = new Random((int)DateTime.Now.Ticks);//thanks to McAden
+        private string RandomString(int size)
+        {
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ INotifyPropertyChangedCallerMemberName/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INotifyPropertyChangedCallerMemberName
+{
+    public class Pessoa : System.ComponentModel.INotifyPropertyChanged
+    {
+        private string _nome;
+        public string Nome 
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                _nome = value;
+                RaisePropertyChanged();
+            }
+        }
+        private string _sobrenome;
+        public string Sobrenome
+        {
+            get
+            {
+                return _sobrenome;
+            }
+            set
+            {
+                _sobrenome = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
